a00ebe78fdb294d233bdb5514e85786df6a897b2
Mokshi Vyas <vyasmokshi31@gmail.com>
2019-10-03 12:04:43 -0700

    added userProfileService java

--- HelloWebService/src/main/java/com/zensar/webservice/hello/HelloService.java
+++ HelloWebService/src/main/java/com/zensar/webservice/hello/HelloService.java
@@ -1,9 +1,13 @@
 package com.zensar.webservice.hello;
 
+import java.time.LocalDate;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class HelloService {
@@ -21,10 +25,36 @@ public class HelloService {
 	{
 		return "<h1> Welcome to Zensar Html </h1>";
 	}
+
 @Path("/xml")
 @GET
 @Produces(MediaType.TEXT_XML)
 	public String sayXmlHello() {
 		return "<?xml version ='1.0'?><hello>Welcome to Zensar XML</hello>";
+	}
+
+	@GET
+	@Path("/{nm}")
+	@Produces("text/plain")
+	public String sayHelloUser(@PathParam("nm") String name) {
+	return "Dear " +name+ " welcome to zensar hello service";
+	}
+	
+	
+	@GET
+	@Path("res/{nm}")
+	@Produces("text/plain")
+	public Response sayHelloUserResponse(@PathParam("nm") String name) {
+		Response res = Response.status(200).entity("Dear " +name+ " welcome to zensar hello service").build();
+	return res;
+	}
+	
+	@GET
+	@Path("res/{nm}/{dd}/{mm}/{yy}")
+	@Produces("text/plain")
+	public Response sayHappyBirthday(@PathParam("nm") String name,@PathParam("dd") int day,@PathParam("mm") int month,@PathParam("yy") int year) {
+		LocalDate birthDate = LocalDate.of(year, month, day);
+		Response res = Response.status(200).entity("Dear " +name+ "  Happy Birthday on "+birthDate +" welcome to zensar hello service").build();
+	return res;
 	}
 }
